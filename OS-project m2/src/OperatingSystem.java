55227fa1831cdc18248ed37d96a4bc747470b183
Reem Salah <47977185+reeemsalah@users.noreply.github.com>
2020-04-21 20:04:37 +0200

    Semaphores and Schedulers are implemented

--- OS-project m2/src/OperatingSystem.java
+++ OS-project m2/src/OperatingSystem.java
@@ -9,150 +9,174 @@ import java.util.Queue;
 import java.util.Scanner;
 import java.util.concurrent.Semaphore;
 
-
 public class OperatingSystem {
-	
+
 	public static ArrayList<Thread> ProcessTable;
-	//Scheduling algorithm: FCFS
+	// Scheduling algorithm: FCFS
 	private static Queue<Thread> readyQ;
-	//Queue for blocked processes
-	private static Queue<Thread>blockedQ;
-	//Blocked Queues foe each resource
+	// Blocked Queues foe each resource
 	private static Queue<Thread> blockedQReadFile;
 	private static Queue<Thread> blockedQWriteFile;
 	private static Queue<Thread> blockedQPrint;
 	private static Queue<Thread> blockedQTextInput;
-	//Semaphores for each resource, initially they are available
-	private static int semReadFile=1;
-	private static int semWriteFile=1;
-	private static int semPrint=1;
-	private static int semTextInput=1;
-	//counter for the number of active processes
-	public static int activeProcess= 0;
-	
-	
-	
-	
-	//system calls:
+	// Semaphores for each resource, initially they are available
+	private static int semReadFile = 1;
+	private static int semWriteFile = 1;
+	private static int semPrint = 1;
+	private static int semTextInput = 1;
+	// counter for the number of active processes
+	public static int activeProcess = 0;
+
+	// system calls:
 	// 1- Read from File
 	@SuppressWarnings("unused")
 	public static String readFile(String name) {
-		String Data="";
+		String Data = "";
 		File file = new File(name);
-	 try {
-		Scanner scan = new Scanner(file);
-		while (scan.hasNextLine())
-		{
-			Data+= scan.nextLine()+"\n";
+		try {
+			Scanner scan = new Scanner(file);
+			while (scan.hasNextLine()) {
+				Data += scan.nextLine() + "\n";
+			}
+			scan.close();
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
 		}
-		scan.close();
-	} catch (FileNotFoundException e) {
-		System.out.println(e.getMessage());
-	}
 		return Data;
 	}
-	
+
 	// 2- Write into file
 	@SuppressWarnings("unused")
 	public static void writefile(String name, String data) {
-		try
-		{
+		try {
 			BufferedWriter BW = new BufferedWriter(new FileWriter(name));
 			BW.write(data);
 			BW.close();
-		} 
-		catch (IOException e) 
-		{
+		} catch (IOException e) {
 			System.out.println(e.getMessage());
 		}
 
 	}
-	//3- print to console
+
+	// 3- print to console
 	@SuppressWarnings("unused")
 	public static void printText(String text) {
 
 		System.out.println(text);
-		
+
 	}
-	
-	//4- take input
-	
+
+	// 4- take input
+
 	@SuppressWarnings("unused")
 	public static String TakeInput() {
-		Scanner in= new Scanner(System.in);
+		Scanner in = new Scanner(System.in);
 		String data = in.nextLine();
 		return data;
-		
+
 	}
-	
-	private static void createProcess(int processID){
+
+	private static void createProcess(int processID) {
 		Process p = new Process(processID);
 		ProcessTable.add(p);
 		readyQ.add(p);
-		Process.setProcessState(p,ProcessState.Ready);
-		//p.start();
-		
+		Process.setProcessState(p, ProcessState.Ready);
+		// p.start();
+
 	}
-	//Semaphores
+
+	// Semaphores
 	/**
 	 * 
 	 * @param p the process that wants to access the take the semaphore
 	 */
-	public static void semPrintWait(Process p)
-	{
-		//TODO to be completed 
+	public static void semPrintWait(Process p) {
+		if (semPrint == 1)
+			semPrint--;
+		else
+			blockedQPrint.add(p);
+
 	}
-	public static void semReadFiletWait(Process p)
-	{
-		//TODO to be completed 
+
+	public static void semReadFiletWait(Process p) {
+		if (semReadFile == 1)
+			semReadFile--;
+		else
+			blockedQReadFile.add(p);
+
 	}
-	public static void semWriteFileWait(Process p)
-	{
-		//TODO to be completed 
+
+	public static void semWriteFileWait(Process p) {
+		if (semWriteFile == 1)
+			semWriteFile--;
+		else
+			blockedQWriteFile.add(p);
+
 	}
-	public static void semTextInputWait(Process p)
-	{
-		//TODO to be completed 
+
+	public static void semTextInputWait(Process p) {
+		if (semTextInput == 1)
+			semTextInput--;
+		else
+			blockedQTextInput.add(p);
 	}
+
 	/**
 	 * 
 	 * @param p the process that wants to post the semaphore
 	 */
-	public static void semPrintPost(Process p)
-	{
-		//TODO to be completed 
+	public static void semPrintPost() {
+		semPrint++;
+		
+		if (!blockedQPrint.isEmpty())
+			readyQ.add(blockedQPrint.remove());
 	}
-	
-	public static void semsReadFilePost(Process p)
-	{
-		//TODO to be completed 
+
+	public static void semReadFilePost() {
+		semReadFile++;
+		
+		if (!blockedQReadFile.isEmpty())
+			readyQ.add(blockedQReadFile.remove());
 	}
-	
-	public static void semWriteFilePost(Process p)
-	{
-		//TODO to be completed 
+
+	public static void semWriteFilePost() {
+		semWriteFile++;
+		if (!blockedQWriteFile.isEmpty())
+			readyQ.add(blockedQWriteFile.remove());
 	}
-	public static void semsemTextInputPost(Process p)
-	{
-		//TODO to be completed 
+
+	public static void semTextInputPost() {
+		semTextInput++;
+		if (!blockedQTextInput.isEmpty())
+			readyQ.add(blockedQTextInput.remove());
 	}
-	
+
+	/**
+	 * Scheduler is an algorithm used to schedule between processes to ensure that
+	 * all processes get to execute according to FCFS (First Come first Serve)
+	 */
+	public static void scheduler() {
+		while (!readyQ.isEmpty() ) {
+			Process p=(Process)readyQ.remove();
+			p.run();
+
+		}
+
+	}
+
 	public static void main(String[] args) {
-   		ProcessTable = new ArrayList<Thread>();
-   		readyQ=new LinkedList<Thread>();
-   		blockedQ=new LinkedList<Thread>();
-   		blockedQPrint=new LinkedList<Thread>();
-   		blockedQReadFile=new LinkedList<Thread>();
-   		blockedQTextInput=new LinkedList<Thread>();
-   		blockedQWriteFile=new LinkedList<Thread>();
+		ProcessTable = new ArrayList<Thread>();
+		readyQ = new LinkedList<Thread>();
+		blockedQPrint = new LinkedList<Thread>();
+		blockedQReadFile = new LinkedList<Thread>();
+		blockedQTextInput = new LinkedList<Thread>();
+		blockedQWriteFile = new LinkedList<Thread>();
 		createProcess(1);
 		createProcess(2);
 		createProcess(3);
 		createProcess(4);
 		createProcess(5);
+		scheduler();
 
 	}
 }
-
-
-
--- OS-project m2/src/Process.java
+++ OS-project m2/src/Process.java
@@ -1,93 +1,144 @@
 //import java.util.concurrent.Semaphore;
 
-
 public class Process extends Thread {
-	
-	public int processID;
-    ProcessState status=ProcessState.New;	
 
-	
+	public int processID;
+	ProcessState status = ProcessState.New;
+
 	public Process(int m) {
 		processID = m;
 	}
+
 	@Override
 	public void run() {
 		this.setProcessState(this, ProcessState.Running);
-		switch(processID)
-		{
-		case 1:process1();break;
-		case 2:process2();break;
-		case 3:process3();break;
-		case 4:process4();break;
-		case 5:process5();break;
+		switch (processID) {
+		case 1:
+			process1();
+			break;
+		case 2:
+			process2();
+			break;
+		case 3:
+			process3();
+			break;
+		case 4:
+			process4();
+			break;
+		case 5:
+			process5();
+			break;
 		}
 
 	}
-	
+
 	private void process1() {
-		
+
+		OperatingSystem.semPrintWait(this);
 		OperatingSystem.printText("Enter File Name: ");
+		OperatingSystem.semPrintPost();
+
+		OperatingSystem.semReadFiletWait(this);
+		OperatingSystem.semPrintWait(this);
 		OperatingSystem.printText(OperatingSystem.readFile(OperatingSystem.TakeInput()));
-		
-		setProcessState(this,ProcessState.Terminated);
-		}
-	
+		OperatingSystem.semReadFilePost();
+		OperatingSystem.semPrintPost();
+
+		setProcessState(this, ProcessState.Terminated);
+	}
+
 	private void process2() {
-		
+
+		OperatingSystem.semPrintWait(this);
 		OperatingSystem.printText("Enter File Name: ");
-		String filename= OperatingSystem.TakeInput();
+		OperatingSystem.semPrintPost();
+
+		OperatingSystem.semTextInputWait(this);
+		String filename = OperatingSystem.TakeInput();
+		OperatingSystem.semTextInputPost();
+
+		OperatingSystem.semPrintWait(this);
 		OperatingSystem.printText("Enter Data: ");
-		String data= OperatingSystem.TakeInput();
-		OperatingSystem.writefile(filename,data);
-		setProcessState(this,ProcessState.Terminated);
-		}
+		OperatingSystem.semPrintPost();
+
+		OperatingSystem.semTextInputWait(this);
+		String data = OperatingSystem.TakeInput();
+		OperatingSystem.semTextInputPost();
+
+		OperatingSystem.semWriteFileWait(this);
+		OperatingSystem.writefile(filename, data);
+		OperatingSystem.semWriteFilePost();
+
+		setProcessState(this, ProcessState.Terminated);
+	}
+
 	private void process3() {
-		int x=0;
-		while (x<301)
-		{ 
-			OperatingSystem.printText(x+"\n");
+		int x = 0;
+		while (x < 301) {
+			OperatingSystem.semPrintWait(this);
+			OperatingSystem.printText(x + "\n");
+			OperatingSystem.semPrintPost();
+			
 			x++;
 		}
-		setProcessState(this,ProcessState.Terminated);
-		}
-	
+		setProcessState(this, ProcessState.Terminated);
+	}
+
 	private void process4() {
-	
-		int x=500;
-		while (x<1001)
-		{
-			OperatingSystem.printText(x+"\n");
+
+		int x = 500;
+		while (x < 1001) {
+			OperatingSystem.semPrintWait(this);
+			OperatingSystem.printText(x + "\n");
+			OperatingSystem.semPrintPost();
+
 			x++;
-		}	
-		setProcessState(this,ProcessState.Terminated);
 		}
+		setProcessState(this, ProcessState.Terminated);
+	}
+
 	private void process5() {
-		
+
+		OperatingSystem.semPrintWait(this);
 		OperatingSystem.printText("Enter LowerBound: ");
-		String lower= OperatingSystem.TakeInput();
-		OperatingSystem.printText("Enter UpperBound: ");
-		String upper= OperatingSystem.TakeInput();
-		int lowernbr=Integer.parseInt(lower);
-		int uppernbr=Integer.parseInt(upper);
-		String data="";
+		OperatingSystem.semPrintPost();
+
+		OperatingSystem.semTextInputWait(this);
+		String lower = OperatingSystem.TakeInput();
+		OperatingSystem.semTextInputPost();
 		
-		while (lowernbr<=uppernbr)
-		{
-			data+=lowernbr++ +"\n";
-		}	
+		OperatingSystem.semPrintWait(this);
+		OperatingSystem.printText("Enter UpperBound: ");
+		OperatingSystem.semPrintPost();
+		
+		OperatingSystem.semTextInputWait(this);
+		String upper = OperatingSystem.TakeInput();
+		OperatingSystem.semTextInputPost();
+
+		int lowernbr = Integer.parseInt(lower);
+		int uppernbr = Integer.parseInt(upper);
+		String data = "";
+
+		while (lowernbr <= uppernbr) {
+			data += lowernbr++ + "\n";
+		}
+		
+		OperatingSystem.semWriteFileWait(this);
 		OperatingSystem.writefile("P5.txt", data);
-		setProcessState(this,ProcessState.Terminated);
+		OperatingSystem.semWriteFilePost();
+		
+		setProcessState(this, ProcessState.Terminated);
 	}
-	
-	 public static void setProcessState(Process p, ProcessState s) {
-		 p.status=s;
-		 if (s == ProcessState.Terminated)
-		 {
-			 OperatingSystem.ProcessTable.remove(OperatingSystem.ProcessTable.indexOf(p));
-		 }
+
+	public static void setProcessState(Process p, ProcessState s) {
+		p.status = s;
+		if (s == ProcessState.Terminated) {
+			OperatingSystem.ProcessTable.remove(OperatingSystem.ProcessTable.indexOf(p));
+		}
+		
 	}
-	 
-	 public static ProcessState getProcessState(Process p) {
-		 return p.status;
+
+	public static ProcessState getProcessState(Process p) {
+		return p.status;
 	}
 }
